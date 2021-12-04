d371bd86578dac42cf371fd172aa6649f932ee18
Michel Antunes Queiroz <michelantunezqueiroz@gmail.com>
2021-12-04 13:26:01 -0300

    Navbar, tabela de serviços e campo de busca

--- Front/SalonManagement-App/src/app/app.module.ts
+++ Front/SalonManagement-App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +8,24 @@ import { AppComponent } from './app.component';
 import { ServicosComponent } from './servicos/servicos.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServicosComponent, ProdutosComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    ServicosComponent,
+    ProdutosComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CollapseModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Front/SalonManagement-App/src/app/servicos/servicos.component.ts
+++ Front/SalonManagement-App/src/app/servicos/servicos.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicos.component.scss'],
 })
 export class ServicosComponent implements OnInit {
-  public servicos: any;
+  public servicos: any = [];
+  private _filtroTabela: string = '';
+  public servicosFiltrados: any = [];
+
+  public get filtroTabela(): string {
+    return this._filtroTabela;
+  }
+
+  public set filtroTabela(value: string) {
+    this._filtroTabela = value;
+    this.servicosFiltrados = this.filtroTabela
+      ? this.filtrarServicos(this.filtroTabela)
+      : this.servicos;
+  }
+
+  public filtrarServicos(filtro: string): any {
+    filtro = filtro.toLocaleLowerCase();
+    return this.servicos.filter(
+      (servico: any) =>
+        servico.cliente.toLocaleLowerCase().indexOf(filtro) !== -1 ||
+        servico.dataServico.toLocaleLowerCase().indexOf(filtro) !== -1
+    );
+  }
+
   constructor(private clienteHttp: HttpClient) {}
 
   ngOnInit(): void {
@@ -16,7 +39,9 @@ export class ServicosComponent implements OnInit {
 
   public getServicos(): void {
     this.clienteHttp.get('https://localhost:5001/api/servicos').subscribe({
-      next: (resposta: any) => (this.servicos = resposta),
+      next: (resposta: any) => {
+        (this.servicos = resposta), (this.servicosFiltrados = this.servicos);
+      },
       error: (erro: any) => console.error(erro),
       complete: () => {},
     });
